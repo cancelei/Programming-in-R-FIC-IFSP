f26a464758aac64deaa6f28d31f38b90a775beb4
Glauber <glauber.bannwart@gmail.com>
2023-09-04 20:51:48 -0300

    update exercise 5 using vector

--- 25.08.2023/glauber_bannwart_05.r
+++ 25.08.2023/glauber_bannwart_05.r
@@ -1,12 +1,26 @@
 # Solicite ao usuário a entrada de um número e mostre a tabuada desse número. O 
 # número digitado pelo usuário deve estar entre 2 e 20
 
+# getnumero = as.numeric(readline(prompt="Digite um número: "));
+# if (getnumero < 2 | getnumero > 20) {
+#     print("Digite um número entre 2 e 20");
+#     getnumero = as.numeric(readline(prompt="Digite um número: "));
+# } else {
+#     for (i in 1:10) {
+#         print(paste(getnumero, "x", i, "=", getnumero * i));
+#     }
+# }
+
+# Using vector
+
 getnumero = as.numeric(readline(prompt="Digite um número: "));
 if (getnumero < 2 | getnumero > 20) {
     print("Digite um número entre 2 e 20");
     getnumero = as.numeric(readline(prompt="Digite um número: "));
 } else {
+    tabuada = c();
     for (i in 1:10) {
-        print(paste(getnumero, "x", i, "=", getnumero * i));
+        tabuada = c(tabuada, paste(getnumero, "x", i, "=", getnumero * i));
     }
+    print(tabuada);
 }
